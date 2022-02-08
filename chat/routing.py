51d820f4a9b4601ff8891614cfe14d08f6ab0968
shaheen2013 <shaheenmediusware@gmail.com>
2022-02-09 01:10:19 +0600

    primary chat application done

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.urls import re_path, path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
 ]
