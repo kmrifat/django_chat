12b9c0053d2763cb996f95fecdc5e214346e69de
kmrifat <kmrifat@gmail.com>
2022-05-31 13:57:43 +0600

    addes ssl in localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    server: {
+        https: true,
+        host: true
+    },
+    plugins: [vue(), mkcert()]
 })
--- server/config/settings.py
+++ server/config/settings.py
@@ -149,8 +149,9 @@ CHANNEL_LAYERS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'https://localhost:3000',
+    'https://127.0.0.1:3000',
+    'https://192.168.0.107:3000'
 ]
 
 INTERNAL_IPS = [
